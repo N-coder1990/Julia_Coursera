963fdd5c354d6e114784e2f00d7b30de40ec435f
N-coder1990 <69002516+N-coder1990@users.noreply.github.com>
2020-07-30 17:54:58 +0300

    Delete working_1.jl

--- working_1.jl
+++
@@ -1,8 +0,0 @@
-using DelimitedFiles
-wikiEVDraw_check1 = DelimitedFiles.readdlm("wikipediaEVDraw.csv", ',')
-
-col1 = wikiEVDraw[:, 1] ;
-
-for i = 1:length(col1)
-    col1[i] = Dates.DateTime(col1[i], "d u y")  # note that this replaces the previous value in col1[i]
-end
